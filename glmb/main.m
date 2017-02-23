c1ce369bdaae1860334a373d9d24f2a1d3aac8d5
yuxuanx <xiayuxuan1993@outlook.com>
2017-02-23 07:20:11 +0100

    minor changes

--- glmb/main.m
+++ glmb/main.m
@@ -2,23 +2,27 @@ clc;clear
 dbstop if error
 
 numTrial = 100;
-K = 101;
+K = 100;
 % GOSPA parameters
 gospa_p= 1;
 gospa_c= 100;
 gospa_alpha= 2;
 gospa_vals= zeros(K,4,numTrial);
-model= gen_model2(0.75,10);
+
+model= gen_model(0.75,30);
+load('truth_30_75');
+load('meas_30_75');
+
 
 parfor trial = 1:numTrial
     
-    truth= gen_truth2(model);
-    meas=  gen_meas(model,truth);
-    est=   run_filter(model,meas);
+%     truth= gen_truth(model);
+%     meas=  gen_meas(model,truth);
+    est=   run_filter(model,meas{trial});
     
     for k=1:K
         [gospa_vals(k,:,trial)] = ...
-            gospa_dist(get_comps(truth.X{k},[1 3]),get_comps(est.X{k},[1 3])...
+            gospa_dist(get_comps(truth{trial}.X{k},[1 3]),get_comps(est.X{k},[1 3])...
             ,gospa_c,gospa_p,gospa_alpha);
     end
 end
